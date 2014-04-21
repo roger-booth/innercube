1be277aca93aa1efcec0366f91910cb31010125a
rwbogy <roger.booth@gmail.com>
2014-04-20 21:50:11 -0400

    Extend painful exercise to test syntax for edges.

--- transformations.go
+++ transformations.go
@@ -28,4 +28,9 @@ func main() {
     faceMap1 := make(map[Color]Face)
     faceMap1["red"] = face1
     cube1.faceMap = faceMap1
+    edge1 := Edge {&face1[0], &face1[0], &face1[0], &face1[0], &face1[0], &face1[0],
+                   &face1[0], &face1[0], &face1[0], &face1[0], &face1[0], &face1[0]}
+    edgeMap1 := make(map[Color]Edge)
+    edgeMap1["red"] = edge1
+    cube1.edgeMap = edgeMap1
 }
