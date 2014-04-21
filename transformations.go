1c30093376e705b345a01a42fab1bb8385e34ed3
rwbogy <roger.booth@gmail.com>
2014-04-21 14:47:16 -0400

    Show that NewCube works.

--- transformations.go
+++ transformations.go
@@ -28,7 +28,7 @@ type Cube struct {
 	edgeMap map[Color]Edge
 }
 
-func New() (*Cube, error) {
+func NewCube() (*Cube, error) {
 	newFaceMap := make(map[Color]*Face)
 	newEdgeMap := make(map[Color]Edge)
 	for _, color := range colors {
@@ -38,10 +38,15 @@ func New() (*Cube, error) {
 	for _, faceColor := range colors {
 		var newEdge Edge
 		for _, edgeColor := range edgesForFace[faceColor] {
+		        //fmt.Println(faceColor)
+		        //fmt.Println(i)
 			newEdge[i] = &newFaceMap[edgeColor][edgePos[i]]
 			newEdge[i+1] = &newFaceMap[edgeColor][edgePos[i+1]]
 			newEdge[i+2] = &newFaceMap[edgeColor][edgePos[i+2]]
 			i += 3
+			if i == 12 {
+			    i = 0
+			}
 		}
 		newEdgeMap[faceColor] = newEdge
 	}
@@ -54,21 +59,13 @@ type ThreeDTransformer struct {
 }
 
 func main() {
-	cube1 := new(Cube)
-	face1 := &Face{"red", "red", "red", "red", "red", "red", "red", "red"}
-	faceMap1 := make(map[Color]*Face)
-	faceMap1["red"] = face1
-	cube1.faceMap = faceMap1
-	edge1 := Edge{&face1[0], &face1[1], &face1[2], &face1[3], &face1[4], &face1[5],
-		&face1[6], &face1[7], &face1[0], &face1[1], &face1[2], &face1[3]}
-	edgeMap1 := make(map[Color]Edge)
-	edgeMap1["red"] = edge1
-	cube1.edgeMap = edgeMap1
-	*cube1.edgeMap["red"][0] = "blue"
-	*cube1.edgeMap["red"][1] = "green"
-	fmt.Println(cube1.faceMap["red"][0])
+	cube1,_ := NewCube()
+
+	//fmt.Println(cube1)
 	fmt.Println(cube1.faceMap["red"][1])
 	fmt.Println(cube1.faceMap["red"][2])
 	fmt.Println(*cube1.edgeMap["red"][2])
+	fmt.Println(*cube1.edgeMap["red"][3])
+	fmt.Println(*cube1.edgeMap["red"][8])
+	fmt.Println(*cube1.edgeMap["red"][11])
 }
-
